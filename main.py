29bf0fb435b04f0687d708e816f14bead3e44ecf
hippolytecosserat <hippolytecosserat@gmail.com>
2022-11-19 18:34:38 +0100

    Test commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from cours.tipe import simulation
+
+sim = simulation.Simulation(1440, 840, logging_level=3)
+
+cf = simulation.CarFactory(["rand_color", "rand_length", "{'a': 20}"], lambda t: round(t, 2) % 3 == 0)
+
+road_list = [{"obj_id": 1, "start": (0, 400), "end": (1200, 550), "car_factory": cf},
+             {"obj_id": 2, "start": (1188, 550), "end": (1188, 0)},
+             {"obj_id": 3, "start": (1188, 550), "end": (1188, 840)}]
+
+road_graph = {1: {2: 0.3, 3: 0.7}, 2: None, 3: None}
+
+sim.create_roads(road_list)
+sim.set_road_graph(road_graph)
+
+sim.start_loop()
---
+++ mathsandutils.py
@@ -0,0 +1,72 @@
+import numpy as np
+from colorama import Fore
+import json
+
+
+globvar = {}
+
+
+def vect_dir(start, end):
+    """Renvoyer le vecteur directeur normé d'une droite"""
+    x1, y1 = start
+    x2, y2 = end
+    a, b = x2 - x1, y2 - y1
+    n = norme((a, b))
+    return a/n, b/n
+
+
+def norme(vect):
+    """Norme du vecteur"""
+    a, b = vect
+    return np.sqrt(a*a + b*b)
+
+
+def vect_norm(vecteur, nouv_norme=1):
+    """Vecteur normal et normé à un autre vecteur"""
+    a, b = vecteur
+    coeff = nouv_norme/norme(vecteur)
+    return -b*coeff, a*coeff
+
+
+def angle(vect):
+    """Angle du vecteur, en degrés"""
+    a, b = vect
+    return -np.degrees(np.angle(a+1j*b))
+
+
+def wc_to_rc(x, y, size):
+    """Convertit des coordonnées pygame en coordonnées classiques"""
+    return x, size[1] - y
+
+
+def rc_to_wc(x, y, size):
+    """Convertit des coordonnées classiques en coordonnées pygame"""
+    return x, size[1] - y
+
+
+def coord_mvm_apres_dt(d, v=1, a=0, dt=1/60):
+    d = d + v*dt + 1/2*a*dt*dt  # dev de taylor ordre 2
+    v = v + a*dt  # dev de taylor ordre 1
+    return d, v, a
+
+
+def rec_round(obj, prec=None):
+    if isinstance(obj, list) or isinstance(obj, tuple):
+        res = []
+        for e in obj:
+            res.append(rec_round(e, prec))
+        if isinstance(obj, tuple):
+            res = tuple(res)
+        return res
+    else:
+        return round(obj, prec)
+
+
+def log(string: str, level: int = 2):
+    """
+    Affiche un message
+    :param string: message
+    :param level: 0 = essentiel, 1 = erreur, 2 = info, 3 = debug
+    """
+    if level <= globvar["logging_level"]:
+        print([Fore.CYAN, Fore.RED, "", Fore.LIGHTBLACK_EX][level] + string + Fore.RESET)
