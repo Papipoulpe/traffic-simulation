28ef502126f1a3026053c5026ecac8692da1d031
hippolytecosserat <hippolytecosserat@gmail.com>
2023-01-05 23:19:04 +0100

    doc dans __init__.py

--- trafficsimulation/__init__.py
+++ trafficsimulation/__init__.py
@@ -1,4 +1,4 @@
 from .simulation import Simulation
 from .objets import CarFactory, TrafficLight
 
-print("\nBienvenue sur Traffic Simulation !")
+print("\nBienvenue sur Traffic Simulation !\n\nESPACE : mettre en pause, FLÈCHE DROITE : ralentir, FLÈCHE GAUCHE : accélérer, ENTRER : recentrer")
